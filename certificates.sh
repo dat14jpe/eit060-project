943513b68ff9628be1cfbe21f95028304f82adaf
dat14jpe <dat14jpe@student.lu.se>
2017-02-02 20:24:28 +0100

    Different client/server CN for certificate script.

--- certificates.sh
+++ certificates.sh
@@ -19,17 +19,19 @@ skeypass="password"     # protects server private key
 groupcn="John Helbrink (mat14jhe)/Simon Johansson (tpi13sjo)/Johan Pettersson (dat14jpe)/Otto Sörnäs (dic15oso)"
 dnameca="/C=SE/ST=Scania/L=Lund/O=LTH/OU=Student/CN=CA"
 dnamec="CN=\"${groupcn}\",OU=Student,O=LTH,L=Lund,S=Scania,C=SE"
+dnames="CN=MyServer,OU=Student,O=LTH,L=Lund,S=Scania,C=SE"
 
 # Create CA certificate and key, stored in separate files.
 openssl req -x509 -newkey rsa:4096 -keyout cakey.pem -out cacert.pem -passout pass:${capass} -days 365 -subj $dnameca
 
 create_stores_and_certificate()  {
     alias=$1
-    tspass=$2
-    kspass=$3
-    keypass=$4
-    tstore=$5
-    kstore=$6
+    dname=$2
+    tspass=$3
+    kspass=$4
+    keypass=$5
+    tstore=$6
+    kstore=$7
 
     # Delete truststore/keystore, if they exist.
     rm $tstore 2> /dev/null
@@ -39,7 +41,7 @@ create_stores_and_certificate()  {
     keytool -importcert -alias ca -file cacert.pem -keystore $tstore -storepass $tspass -noprompt
 
     # Create our keypair (and keystore).
-    keytool -genkeypair -alias $alias -keyalg rsa -keysize 4096 -keystore $kstore -storepass $kspass -keypass $keypass -dname "$dnamec"
+    keytool -genkeypair -alias $alias -keyalg rsa -keysize 4096 -keystore $kstore -storepass $kspass -keypass $keypass -dname "$dname"
 
     # Create our certificate request.
     keytool -certreq -alias $alias -file clientcsr -keystore $kstore -storepass $kspass
@@ -52,8 +54,8 @@ create_stores_and_certificate()  {
     keytool -importcert -alias $alias -file clientcert.pem -keystore $kstore -storepass $kspass -noprompt
 }
 
-create_stores_and_certificate client $ctspass $ckspass $ckeypass clienttruststore clientkeystore
-create_stores_and_certificate server $stspass $skspass $skeypass servertruststore serverkeystore
+create_stores_and_certificate client "$dnamec" $ctspass $ckspass $ckeypass clienttruststore clientkeystore
+create_stores_and_certificate server "$dnames" $stspass $skspass $skeypass servertruststore serverkeystore
 
 # List client certificates.
 #keytool -list -v -keystore clientkeystore -storepass $ckspass
